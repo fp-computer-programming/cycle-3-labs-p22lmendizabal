3c39b80a45d3115881b13b01be714a332f3159bd
p22lmendizabal <p22lmendizabal@fairfieldprep.org>
2021-09-28 10:04:45 -0400

    Submit cycle3 day 2 Labs

---
+++ lab_2-1.py
@@ -0,0 +1,4 @@
+# author: LM (AMDG) 09/28/2021
+x = input("Please enter a number: ")
+if int(x) % 2 == 0:
+    print(x + " is an even number.")
---
+++ lab_2-2.py
@@ -0,0 +1,6 @@
+# author: LM (AMDG) 09/28/2021
+x = input("Please enter a number: ")
+if int(x) % 2 == 0:
+    print(x + " is an even number.")
+else: 
+    print(x + " is a negative number.")
