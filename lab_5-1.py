276d2d7204ebbac3eb16f365ac53088055bf1c1f
p22lmendizabal <p22lmendizabal@fairfieldprep.org>
2021-10-04 09:57:06 -0400

    cycle3 day 5 lab

---
+++ lab_5-1.py
@@ -0,0 +1,16 @@
+# author : LM (AMDG) 10/04/21
+
+#int(a) 
+# name error
+
+#int("a") 
+# a is not a number that is defined. Integers do not use quotes
+
+# "a" + 2 
+#str and integer are not the same type
+
+#import date 
+# there is no module named date
+
+# print("I am a happy camper.") 
+# syntax error
